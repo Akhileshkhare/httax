195ae1045ef23cef7cb80f901481deb7ab8c24b6
akhilesh khare <akhileshkhare.work@gmail.com>
2024-11-07 21:55:59 +0530

    change auth config

--- auth.js
+++ auth.js
@@ -1,20 +1,27 @@
-// auth.js
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const authenticateToken = (req, res, next) => {
   const token = req.header("Authorization")?.split(" ")[1];
-  if (!token)
-    return res
-      .status(401)
-      .json({ message: "Access denied. No token provided." });
+  if (!token) {
+    return res.status(401).json({ message: "Access denied. No token provided." });
+  }
 
   try {
     const verified = jwt.verify(token, process.env.JWT_SECRET);
     req.user = verified;
     next();
   } catch (error) {
-    res.status(400).json({ message: "Invalid token." });
+    if (error instanceof jwt.TokenExpiredError) {
+      // Handle token expiration
+      res.status(401).json({ message: "Token has expired." });
+    } else if (error instanceof jwt.JsonWebTokenError) {
+      // Handle invalid token
+      res.status(400).json({ message: "Invalid token." });
+    } else {
+      // Handle other errors
+      res.status(400).json({ message: "Authentication error." });
+    }
   }
 };
 
